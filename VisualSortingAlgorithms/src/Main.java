7e8e4d8c5593dd5bd9c4c16006839d9e853cc813
Sultan <84280810+sultanAliyev@users.noreply.github.com>
2022-07-19 10:38:13 +0600

    Add files via upload

---
+++ VisualSortingAlgorithms/src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage) {
+
+    AnimationController animationController = new AnimationController();
+    animationController.setStyle("-fx-background-color: #ffffff");
+
+    Scene scene = new Scene(animationController,
+                            AnimationController.WINDOW_WIDTH,
+                            AnimationController.WINDOW_HEIGHT);
+
+    stage.setTitle("Visual Sorting Algorithms");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+}
